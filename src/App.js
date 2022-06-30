4fe5d38907795287f37c5a14b7a172aa39dbeb71
Sarb0z <abdulrafay972@gmail.com>
2022-06-30 23:05:37 +0500

    Integrated api with search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./Components/SearchBar";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState(query || '');
+  const [artistData, setArtistData]= useState([]);
+  
+  useEffect(() => {
+    fetch("https://rest.bandsintown.com/artists/$`{artistname}`/?app_id=123")
+    .then(response => response.json())
+        // 4. Setting *dogImage* to the image url that we received from the response above
+    .then(data => setArtistData(data.message))
+  },[])
+  
   return (
     <div className="App">
-      <SearchBar/>
+      <SearchBar
+      searchTerm={searchTerm}
+      setSearchTerm={setSearchTerm}/>
     </div>
   );
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,17 +4,26 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useHistory } from 'react-router-dom';
 
-const SearchBar = () => {
+
+const SearchBar = ({searchterm, setSearchTerm}) => {
+    const history=useHistory();
+    const onSubmit = (e) => {
+        history.push(`?s=${searchTerm}`);
+        e.preventDefault();
+    };
     return (
-    <Form>
+    <Form onSubmit={onSubmit}>
     <InputGroup className="col-6">
           <FormControl
             placeholder="Search"
             aria-label="Search"
             aria-describedby="basic-addon2"
+            value={searchTerm}
+            onInput={(e) => setSearchTerm(e.target.value)}
           />
-          <Button variant="outline-secondary" id="button-addon2">
+          <Button variant="outline-secondary" id="button-addon2" type='submit'>
             Search
           </Button>
     </InputGroup>
