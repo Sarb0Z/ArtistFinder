bed530da1ec5c8c4ef27b6e460545322986041f7
Sarb0z <abdulrafay972@gmail.com>
2022-07-01 03:42:58 +0500

    Api link established

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, setError } from "react";
 import SearchBar from "./Components/SearchBar";
 import axios from "axios"
-
-
-
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [artistData, setArtistData]= useState([]);
+  const controllerRef = useRef(new AbortController());
+
   useEffect(() => {
-    
-    axios.get(`https://rest.bandsintown.com/artists/${searchTerm}/?app_id=abc`
-    , {
-      headers: {"Access-Control-Allow-Origin": "*"}
-    })
-    .then((response) => console.log(response.data));
-  }, []);
+    (async () => {
+      try {
+        const response = await axios.request({
+          signal: controllerRef.current.signal,
+          method: "GET",
+          url: `https://rest.bandsintown.com/artists/${searchTerm}/?app_id=abc`,
+        });
+        setArtistData(response.data.name);
+      } catch (error) {
+        setError(error);
+      } 
+    })();
+  }, [searchTerm]);
   return (
     <div className="App">
       <SearchBar
