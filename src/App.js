ad985a4b223d068e37e377e842fbaeebe5712d97
Sarb0z <abdulrafay972@gmail.com>
2022-07-01 07:32:11 +0500

    Added Event Buttons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [artistData, setArtistData] = useState([]);
+  const [eventData, setEventData] = useState([]);
   const [flag, setFlag] = useState(false);
   useEffect(() => {
     (async () => {
@@ -23,10 +24,34 @@ function App() {
       }
     })();
   }, [searchTerm]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const event = await axios.request({
+          method: "GET",
+          url: `https://rest.bandsintown.com/artists/${artistData.name}/events/?app_id=abc`,
+        });
+        setEventData(event.data);
+      } catch (error) {
+        setError(error);
+      }
+    })();
+  }, [flag]);
+
   return (
     <div className="App">
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       {searchTerm ? <ArtistCard artist={artistData} setFlag={setFlag} /> : null}
+      {flag && searchTerm ? (
+        <ul>
+          {eventData.map((r) => (
+            <li key={r.id}>
+              {r.venue.name} - {r.description}
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 }
--- src/Components/artistCard.js
+++ src/Components/artistCard.js
@@ -1,16 +1,18 @@
 import "./artistCard.css";
 import { Button } from "react-bootstrap";
 const ArtistCard = (props) => {
-
   return (
     <div className="card">
       <img src={props.artist.image_url} alt="" />
       <div className="card-body">
         <h2>{props.artist.name}</h2>
-        <p>{props.artist.facebook_page_url}</p>
-        <Button variant="secondary" size="lg" >
-        Find Events
-      </Button>
+        <p>Upcoming events: {props.artist.upcoming_event_count}</p>
+        <Button variant="secondary" size="lg" onClick={props.setFlag}>
+          Find Events
+        </Button>
+        <Button variant="success" size="lg" onClick={() => window.open(props.artist.url, '_blank')}>
+          Go To Page
+        </Button>
       </div>
     </div>
   );
