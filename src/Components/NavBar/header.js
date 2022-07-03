bf03ab857a0c8d2b9ae3227c576e3c7f3d0d6c74
Sarb0z <abdulrafay972@gmail.com>
2022-07-03 07:46:54 +0500

    deployed to gh-pages

--- src/Components/NavBar/header.js
+++ src/Components/NavBar/header.js
@@ -1,36 +1,34 @@
 import { useState } from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const Header = () => {
-  const [flag, setFlag]= useState(false);
+  const [today, setToday]=useState(false);
+  const [upcoming, setUpcoming]=useState(false);
+  const [date, setDate] = useState(new Date());
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Find Artists!</Navbar.Brand>
+        <Navbar.Brand href="/">Find Artists!</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Today</Nav.Link>
-            <Nav.Link href="#home">Upcoming</Nav.Link>
+            <Nav.Item value={today} onClick={setToday}>Today</Nav.Item>
+              <Nav.Item value={upcoming} onClick={setUpcoming}>Upcoming</Nav.Item>
             <NavDropdown title="Choose Date" id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={()=>setFlag(!flag)}>
+              <NavDropdown.Item >
                 Choose Date
-                <Calendar/>
+                <Calendar onChange={setDate} value={date} />
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Pick Range
-              </NavDropdown.Item>
+              <NavDropdown.Item>Pick Range</NavDropdown.Item>
             </NavDropdown>
-            
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
-
 
 export default Header;
